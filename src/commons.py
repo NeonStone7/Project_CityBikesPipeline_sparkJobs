60fe404aa248b1212789b1e155cf6f1a42433b0b
my_user <oamenmodupe@gmail.com>
2025-06-13 11:25:22 +0100

    added intial processor

---
+++ src/commons.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+
+def local_spark_session(name):
+    return (
+        SparkSession.builder
+        .config('hive.exec.dynamic.partition.mode', 'nonstrict')
+        .config('spark.sql.source.partitionOverwriteMode', 'dynamic')
+        .config('spark.master', 'local[*]')
+        .appName(name)
+        .getOrCreate()
+    )
+
+def remote_spark_session(name):
+    return (
+        SparkSession.builder
+        .config('hive.exec.dynamic.partition.mode', 'nonstrict')
+        .config('spark.sql.source.partitionOverwriteMode', 'dynamic')
+        .config('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions')
+        .config('spark.sql.catalog.spark_catalog', 'org.apache.iceberg.spark.SparkSessionCatalog')
+        .config('spark.sql.catalog.spark_catalog.catalog-impl', 'org.apache.iceberg.aws.glue.GlueCatalog')
+        .config('spark.sql.catalog.spark_catalog.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
+        .config('spark.sql.legacy.timeParserPolicy', 'LEGACY')
+        .config('spark.sql.sources.partitionColumnTypeInference.enabled', 'false')
+        .config('spark.sql.catalog.spark_catalog.http-client.apache.max-connections', '3000')
+        .appName(name)
+        .getOrCreate()
+    )
+
+def save_to_iceberg(spark_session, df, table_name, schema, fields, location, partition = 'year,month,day', catalog='spark_catalog'):
+
+    # create schema
+    spark_session.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}")
+
+    # createtable
+    
+    new_fields = ""
+    for field, dtype in fields.items():
+        new_fields = new_fields + f"{field} {dtype}, "
+    cmd = (
+        f"CREATE EXTERNAL TABLE IF NOT EXISTS {catalog}.{schema}.{table_name}"
+        f" ({new_fields}) " + 
+        f" USING ICEBERG PARTITIONED BY ({partition}) LOCATION '{location}'"
+    )
+    spark_session.sql(cmd)
+
+    # write into table
+    df.write.insertInto(f'{catalog}.{schema}.{table_name}')
---
+++ src/definitions.py
@@ -0,0 +1,31 @@
+from initial_processor import initial_processing
+
+
+definitions = {
+    'initial_network_stations': {
+        'transformer': initial_processing,
+        'schema': 'initial_processed',
+        'fields': {
+            'network_id': 'string',
+            'network_name': 'string',
+            'network_latitude': 'double',
+            'network_longitude': 'double',
+            'network_city': 'string',
+            'network_country': 'string',
+            'network_href': 'string',
+            'network_company': 'string',
+            'network_gbfs_href': 'string',
+            'station_id': 'string',
+            'station_name': 'string',
+            'station_latitude': 'double',
+            'station_longitude': 'double',
+            'station_timestamp': 'timestamp',
+            'station_free_bikes': 'int',
+            'station_empty_slots': 'int',
+            'processing_timestamp': 'timestamp',
+            'year': 'int',
+            'month': 'int',
+            'day': 'int'
+        }
+    }
+}
